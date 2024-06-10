02303f64ea0bad662985a6fa422864bf5c3fc43e
Nihaoyacatie <167575409+nihaoyacatie@users.noreply.github.com>
2024-06-09 22:17:09 -0700

    Initial commit

---
+++ src/main/java/fdu/capstone/AppMain.java
@@ -0,0 +1,16 @@
+package fdu.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Liping Yin
+ * Date: 2024/6/5
+ */
+@SpringBootApplication
+public class AppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class,args);
+    }
+
+}
