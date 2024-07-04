6831f4374dfabc631895775329c01de3057068a7
liping <214458314@qq.com>
2024-07-03 21:49:22 -0700

    1.Amadeus service encapsulates the functionality for querying flight information based on city-to-city searches. 2.chatbot service  integrate amadeus  service

--- src/main/java/fdu/capstone/system/module/controller/AmadeusController.java
+++ src/main/java/fdu/capstone/system/module/controller/AmadeusController.java
@@ -2,6 +2,7 @@ package fdu.capstone.system.module.controller;
 
 import com.amadeus.exceptions.ResponseException;
 import fdu.capstone.system.module.service.impl.AmadeusService;
+import fdu.capstone.system.module.service.impl.AmadeusServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,8 @@ public class AmadeusController {
     @Autowired
     private AmadeusService amadeusService;
 
+    @Autowired
+    private AmadeusServiceImpl amadeusServiceImpl;
     @CrossOrigin(origins = "http://localhost:3000") // Allow requests from localhost:3000
 
     @GetMapping("/locations")
@@ -29,5 +32,13 @@ public class AmadeusController {
                                                      @RequestParam(required = false) String returnDate,
                                                      @RequestParam int adults) throws ResponseException {
         return amadeusService.getFlightOffers(origin, destination, departureDate, returnDate, adults);
+    }
+    @GetMapping("/flightsV2")
+    public List<Map<String, Object>> getFlightOffersV2(@RequestParam String origin,
+                                                     @RequestParam String destination,
+                                                     @RequestParam String departureDate,
+                                                     @RequestParam(required = false) String returnDate,
+                                                     @RequestParam int adults) throws ResponseException {
+        return amadeusServiceImpl.getFlightOffers(origin, destination, departureDate, returnDate, adults);
     }
 }
