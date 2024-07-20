5dad1c73c092a42c5c0b32a5a5804b8fbca7a6f4
Liang Yan <269720@gmail.com>
2024-07-19 22:45:15 -0700

    update Amadeus Service

--- src/main/java/fdu/capstone/system/module/service/impl/AmadeusService.java
+++ src/main/java/fdu/capstone/system/module/service/impl/AmadeusService.java
@@ -13,10 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Type;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class AmadeusService {
